64434264ac9cb931fa79917d8bcc1e630fb64c5d
Christopher Dunn <christopherdunnlaw@gmail.com>
2019-05-08 16:58:26 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+require_relative './spec_helper.rb'
+def fizzbuzz(int)
+    if int % 3 == 0 # if the number int is divisible by 3
+      "Fizz" # Go fizz
+    end
+    else
+        puts int
+    end
+
+puts fizzbuzz(3)
