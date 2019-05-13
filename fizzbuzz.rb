db24f3e2b9857c138d8f22d659820fb10cfd0c4d
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-13 21:33:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
-    
+    puts "Fizz"
+  if int % 5 == 0
+    puts "Buzz"
+  if (int % 5 == 0) && (int % 3 == 0)
+    puts "FizzBuzz"
 
 end
